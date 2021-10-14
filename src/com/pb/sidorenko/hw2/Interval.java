e19fad86bdc776fa2bf2d6bea9de2c93ab9a0b09
20DSidorenko <dn201090kdm@gmail.com>
2021-10-14 17:36:43 +0300

    commit calc commit calc and interval

--- src/com/pb/sidorenko/hw2/Interval.java
+++ src/com/pb/sidorenko/hw2/Interval.java
@@ -1,4 +1,28 @@
 package com.pb.sidorenko.hw2;
 
+import java.util.Scanner;
+
 public class Interval {
+    private static Scanner in;
+
+    public static void main(String[] args) {
+
+        byte a = 0, b = 14, d = 15, e = 35, f = 36, g = 50, h = 51, j = 100;
+
+        System.out.print("\n" + "Введите любое целое число до 100: ");
+
+        in = new Scanner(System.in);
+        byte i = in.nextByte();
+
+        if (i >= a && i <= b) {
+            System.out.print("число " + i + " попадает в интервал от " + a + " до " + b + "\n");
+        } else if (i <= d && i <= e) {
+            System.out.print("число " + i + " попадает в интервал от " + d + " до " + e + "\n");
+        } else if (i <= f && i <= g) {
+            System.out.print("число " + i + " попадает в интервал от " + f + " до " + g + "\n");
+        } else  if (i <= h && i <= j) {
+            System.out.print("число " + i + " попадает в интервал от " + h + " до " + j + "\n");
+        }
+    }
 }
+
